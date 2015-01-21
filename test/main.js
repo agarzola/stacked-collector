9eb41d0833491981f1f78a1565f25cb381fd1634
Alfonso Gómez-Arzola <alfonso@mrkp.co>
2015-01-21 16:37:31 -0500

    Move batcher and cycler to /lib/utils/

--- test/main.js
+++ test/main.js
@@ -1,7 +1,6 @@
 var should = require('should'),
     collector = require('../lib/collector'),
-    batcher = require('../lib/batcher'),
-    cycler = require('../lib/cycler'),
+    util = require('../lib/util'),
     testUsers = require('./data/users'),
     itemProcessor = require('../lib/itemProcessor'),
     userArray, batchedUsers
@@ -17,12 +16,12 @@ describe('User Processor', function () {
 
 describe('Batcher', function () {
   it('should return an array of batches', function (done) {
-    batchedUsers = batcher(userArray, 5)
+    batchedUsers = util.batcher(userArray, 5)
 
     batchedUsers.should.be.an.instanceOf(Array).and.have.lengthOf(3)
-    batchedUsers[0].should.be.an.instanceOf(Array).and.have.lengthOf(5)
-    batchedUsers[1].should.be.an.instanceOf(Array).and.have.lengthOf(5)
-    batchedUsers[2].should.be.an.instanceOf(Array).and.have.lengthOf(1)
+    batchedUsers[0].should.be.an.instanceOf(Object)
+    batchedUsers[1].should.be.an.instanceOf(Object)
+    batchedUsers[2].should.be.an.instanceOf(Object)
     batchedUsers.limit.should.equal(5)
 
     done()
