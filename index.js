be61c68162218933be0eb3bda83a49e2e44835a9
Alfonso Gómez-Arzola <alfonso@mrkp.co>
2015-02-06 18:53:01 -0500

    Adapt collector to use stacked-database

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var collector = require('./lib/collector'),
     async = require('async'),
+    db = require('stacked-database'),
     counter = 1;
 
-require('./db/mongooseSetup');
+db.connect(function (err) {
+  if (err) throw err;
 
-async.forever(
-  function (next) {
-    collector(null, function (err, users) {
-      console.log('called ' + counter + ' times')
-      counter++;
-      next(err, users)
-    })
-  },
-  function (err) {
-    if (err) { throw err; }
-  }
-)
+  async.forever(
+    function (next) {
+      collector(null, function (err, users) {
+        console.log('called ' + counter + ' times')
+        counter++;
+        next(err, users)
+      })
+    },
+    function (err) {
+      if (err) { throw err; }
+    }
+  )
+})
